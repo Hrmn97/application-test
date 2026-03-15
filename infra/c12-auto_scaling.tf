8d8c771a6b93286ef3d22c1f4d5d5159ed222dda
Harman Singh <singh9765h@gmail.com>
2026-03-15 12:39:30 +0530

    modified version

--- infra/c12-auto_scaling.tf
+++ infra/c12-auto_scaling.tf
@@ -1,8 +1,42 @@
 # --- Auto Scaling ---
+# resource "aws_appautoscaling_target" "notification" {
+#   max_capacity       = 4
+#   min_capacity       = 1
+#   resource_id        = "service/${var.ecs_cluster_id}/${aws_ecs_service.notification.name}"
+#   scalable_dimension = "ecs:service:DesiredCount"
+#   service_namespace  = "ecs"
+# }
+
+# resource "aws_appautoscaling_policy" "notification_cpu" {
+#   name               = "${var.environment}-${local.service_name}-cpu-scaling"
+#   policy_type        = "TargetTrackingScaling"
+#   resource_id        = aws_appautoscaling_target.notification.resource_id
+#   scalable_dimension = aws_appautoscaling_target.notification.scalable_dimension
+#   service_namespace  = aws_appautoscaling_target.notification.service_namespace
+
+#   target_tracking_scaling_policy_configuration {
+#     predefined_metric_specification {
+#       predefined_metric_type = "ECSServiceAverageCPUUtilization"
+#     }
+#     target_value = 95.0
+#   }
+# }
+
+
+# --- Auto Scaling ---
+
+# var.ecs_cluster_id is a full ARN like:
+# arn:aws:ecs:us-west-2:123456789:cluster/prod-notification
+# aws_appautoscaling_target resource_id needs just the cluster NAME not the full ARN
+# so we extract it by splitting on "/" and taking the last element
+locals {
+  ecs_cluster_name = element(split("/", var.ecs_cluster_id), length(split("/", var.ecs_cluster_id)) - 1)
+}
+
 resource "aws_appautoscaling_target" "notification" {
   max_capacity       = 4
   min_capacity       = 1
-  resource_id        = "service/${var.ecs_cluster_id}/${aws_ecs_service.notification.name}"
+  resource_id        = "service/${local.ecs_cluster_name}/${aws_ecs_service.notification.name}"
   scalable_dimension = "ecs:service:DesiredCount"
   service_namespace  = "ecs"
 }
@@ -18,6 +52,6 @@ resource "aws_appautoscaling_policy" "notification_cpu" {
     predefined_metric_specification {
       predefined_metric_type = "ECSServiceAverageCPUUtilization"
     }
-    target_value = 95.0
+    target_value = 70.0  # 95 is too high — scale before you're already on fire
   }
 }
--- infra/c6-ecs_iam.tf
+++ infra/c6-ecs_iam.tf
@@ -1,6 +1,56 @@
-# --- IAM Roles ---
-# BUG(S) BELOW: Task execution role and task role
+# # --- IAM Roles ---
+# # BUG(S) BELOW: Task execution role and task role
 
+# resource "aws_iam_role" "task_execution_role" {
+#   name = "${var.environment}-${local.service_name}-execution"
+
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [{
+#       Action = "sts:AssumeRole"
+#       Effect = "Allow"
+#       Principal = {
+#         Service = "ecs-tasks.amazonaws.com"
+#       }
+#     }]
+#   })
+
+#   tags = local.common_tags
+# }
+
+# resource "aws_iam_role_policy_attachment" "task_execution_policy" {
+#   role       = aws_iam_role.task_execution_role.name
+#   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+# }
+
+# resource "aws_iam_role_policy" "secrets_read" {
+#   name = "${var.environment}-${local.service_name}-secrets"
+#   role = aws_iam_role.task_execution_role.id
+
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [{
+#       Effect   = "Allow"
+#       Action   = ["secretsmanager:GetSecretValue"]
+#       Resource = [var.mongodb_secret_arn]
+#     }]
+#   })
+# }
+
+# BUG Found with terraform validate command
+# Error: "policy_arn" (arn:aws:iam::policy/AmazonECSTaskExecutionRolePolicy) is an invalid ARN: arn: not enough sections
+# │ 
+# │   with aws_iam_role_policy_attachment.task_execution_policy,
+# │   on c6-ecs_iam.tf line 23, in resource "aws_iam_role_policy_attachment" "task_execution_policy":
+# │   23:   policy_arn = "arn:aws:iam::policy/AmazonECSTaskExecutionRolePolicy"
+
+# FIX is to use proper arn arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy. Take it from aws console IAM -> Policies -> search "AmazonECSTaskExecutionRolePolicy" -> copy the arn
+
+# Fix: Wrong Principal service ec2.amazonaws.com given. 
+# We need to put ecs-tasks.amazonaws.com as this role will be attached to task definition in ecs so it reuires ecs-tasks instead of ec2.
+
+
+# --- IAM Roles ---
 resource "aws_iam_role" "task_execution_role" {
   name = "${var.environment}-${local.service_name}-execution"
 
@@ -23,8 +73,10 @@ resource "aws_iam_role_policy_attachment" "task_execution_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
+# Required so ECS can pull the MongoDB secret at container startup
+# Without this the task will fail with AccessDeniedException before the container even starts
 resource "aws_iam_role_policy" "secrets_read" {
-  name = "${var.environment}-${local.service_name}-secrets"
+  name = "${var.environment}-${local.service_name}-secrets-read"
   role = aws_iam_role.task_execution_role.id
 
   policy = jsonencode({
@@ -36,15 +88,3 @@ resource "aws_iam_role_policy" "secrets_read" {
     }]
   })
 }
-
-# BUG Found with terraform validate command
-# Error: "policy_arn" (arn:aws:iam::policy/AmazonECSTaskExecutionRolePolicy) is an invalid ARN: arn: not enough sections
-# │ 
-# │   with aws_iam_role_policy_attachment.task_execution_policy,
-# │   on c6-ecs_iam.tf line 23, in resource "aws_iam_role_policy_attachment" "task_execution_policy":
-# │   23:   policy_arn = "arn:aws:iam::policy/AmazonECSTaskExecutionRolePolicy"
-
-# FIX is to use proper arn arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy. Take it from aws console IAM -> Policies -> search "AmazonECSTaskExecutionRolePolicy" -> copy the arn
-
-# Fix: Wrong Principal service ec2.amazonaws.com given. 
-# We need to put ecs-tasks.amazonaws.com as this role will be attached to task definition in ecs so it reuires ecs-tasks instead of ec2.
